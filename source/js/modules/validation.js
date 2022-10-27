baca16a55382b30124dd8eefee16b48606777686
Sergey Troshkin <sergey.troshkin@gmail.com>
2022-10-27 12:21:45 +0300

    Change phone validation

--- source/js/modules/validation.js
+++ source/js/modules/validation.js
@@ -1,45 +1,38 @@
 const validatePhone = () => {
-  const input = document.body.querySelector('#phone');
-
-  input.addEventListener('keypress', (evt) => {
-    if (evt.keyCode < 47 || evt.keyCode > 57) {
-      evt.preventDefault();
+  let eventCalllback = function (e) {
+    let el = e.target;
+    let clearVal = el.dataset.phoneClear;
+    let pattern = el.dataset.phonePattern;
+    let matrixDef = '+7(___) ___-__-__';
+    let matrix = pattern ? pattern : matrixDef;
+    let i = 0;
+    let def = matrix.replace(/\D/g, '');
+    let val = e.target.value.replace(/\D/g, '');
+    if (clearVal !== 'false' && e.type === 'blur') {
+      if (val.length < matrix.match(/([\_\d])/g).length) {
+        e.target.value = '';
+        return;
+      }
     }
-  });
-
-  input.addEventListener('focus', () => {
-    if (input.value.length === 0) {
-      input.value = '+7(';
-      input.selectionStart = input.value.length;
+    if (def.length >= val.length) {
+      val = def;
     }
-  });
-
-  input.addEventListener('click', (evt) => {
-    if (input.selectionStart < 10) {
-      input.selectionStart = input.value.length;
+    e.target.value = matrix.replace(/./g, function (a) {
+      if (/[_\d]/.test(a) && i < val.length) {
+        return val.charAt(i++);
+      } else if (i >= val.length) {
+        return '';
+      } else {
+        return a;
+      }
+    });
+  };
+  let phoneInputs = document.querySelectorAll('[data-phone-pattern]');
+  for (let elem of phoneInputs) {
+    for (let ev of ['input', 'blur', 'focus']) {
+      elem.addEventListener(ev, eventCalllback);
     }
-    if (evt.key === 'Backspace' && input.value.length <= 10) {
-      evt.preventDefault();
-    }
-  });
-
-  input.addEventListener('keydown', () => {
-    if (input.value.length === 6) {
-      input.value += ')';
-    }
-  });
-
-  input.addEventListener('blur', () => {
-    if (input.value <= '+7(' || input.value.length <= 6) {
-      input.value = '';
-    }
-  });
-
-  input.addEventListener('keydown', (evt) => {
-    if (evt.key === 'Backspace' && input.value.length <= 3) {
-      evt.preventDefault();
-    }
-  });
+  }
 };
 
 export {validatePhone};
